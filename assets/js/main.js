2403aa77d8e011a0fda05f89bfed53540dab7052
proma-swarna <promaswarna@gmail.com>
2021-06-17 00:25:30 +0600

    update

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+$(".menu-items").owlCarousel({
+    items : 3,
+    margin : 20,
+    loop : 'true',
+    autoplay : 'true'
+});
+$(".client_review").owlCarousel({
+    items : 1,
+    margin : 20,
+    loop : 'true',
+    autoplay : 'true'
+});
+
+
+
+
+
+
+
+
+
